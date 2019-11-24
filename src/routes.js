474981cb1af434ce6b9373edd7c67cb8d9388597
Gabriel Kukiel <gabrielkukiel@gmail.com>
2019-11-24 16:51:51 -0300

    wrapping

--- src/routes.js
+++ src/routes.js
@@ -71,7 +71,7 @@ const Roles = () => {
   return (
     <div style={{ width: "100%" }}>
       <h1> Lista de roles em {cat}</h1>
-      <div style={{ display: "flex", justifyContent: "space-around", width: "100%" }}>
+      <div style={{ display: "flex", justifyContent: "space-around", flex:"1", flexWrap: "wrap", width: "100%" }}>
         {request && request.map((local) => (
           (local.cat == cat && <Cardd {...local}> </Cardd>)
         ))}
@@ -92,7 +92,7 @@ const Locais = () => {
   return (
     <div style={{ width: "100%" }}>
       <h1> Lista de {cat} disponíveis</h1>
-      <div style={{ display: "flex", justifyContent: "space-around", flexWrap: "wrap", width: "100%" }}>
+      <div style={{ display: "flex", justifyContent: "space-around", flex:"1", flexWrap: "wrap", width: "100%" }}>
         {request && request.map((local) => (
           (local.categoria == cat && <Cardd {...local}> </Cardd>)
         ))}
